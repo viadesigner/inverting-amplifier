a7f3f13c7963d5bc69bb686d9af37496a36e1f2c
Reid Wender <rwender@triadsemi.com>
2013-03-06 16:26:55 -0500

    Added MyFirstOpAmp ViaDesigner project directories
    Design of a simple inverting operational amplifier

---
+++ MyFirstOpAmp/sim_adms/opamp/expt1.cmd
@@ -0,0 +1,17 @@
+* Command file for design: opamp
+* Top Spice file: ..\..\genhdl\opamp\opamp.cir
+.op
+.tran 0 1m 0
+* .ac dec 100  
+.plot tran SG(avdd#defhook#0) V(avdd) V(avss) V(n1n4) V(vcm) V(vin) V(vout)
+* .plot ac V(avdd) V(avss) V(n1n4) V(vcm) V(vin) V(vout)
+.option eps=1.0e-7
+.option hmin=1.0e-12
+.option print_dc=1
+* save tr toplevel
+* save ac toplevel
+* save ba
+
+.save file=opamp.iic end no_save_wdb
+* .plot dc V(avdd) V(avss) V(n1n4) V(vcm) V(vin) V(vout)
+.include ..\..\genhdl\opamp\opamp.cir
